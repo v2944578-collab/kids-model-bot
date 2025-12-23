93158da517684152e00efce7feb8c2ab131a820e
v2944578-collab <v2944578@gmail.com>
2025-12-23 07:37:05 +0700

    Update bot.py
    alive

--- bot.py
+++ bot.py
@@ -1,37 +1,18 @@
 import asyncio
-import os
-
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "ТВОЙ_ТОКЕН_ЗДЕСЬ"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-
 @dp.message(CommandStart())
-async def start(message: Message):
-    await message.answer(
-        "👋 Мы - не просто детское модельное агентство\n\n"
-        "Мы - ALFAkids. Мы помогаем детям раскрыть уверенность, харизму и любовь к сцене ✨\n\n"
-        "📸 Модельная походка\n"
-        "🎭 Актёрское мастерство\n"
-        "🗣 Работа с речью и камерой\n\n"
-        "Выберите нужный раздел в меню ниже ⬇️",
-        reply_markup=main_menu
-    )
-
-
+async def start(message: types.Message):
+    await message.answer("Я жив 🟢")
 
 async def main():
     await dp.start_polling(bot)
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-
-if __name__ == "__main__":
+if name == "__main__":
     asyncio.run(main())
