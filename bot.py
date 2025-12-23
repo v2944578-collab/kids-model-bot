5464e440152ab5b670e5d6767e3068484fccea24
v2944578-collab <v2944578@gmail.com>
2025-12-23 07:50:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,27 @@
-import os
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+import asyncio
+import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
-async def start(message: types.Message):
-    await message.answer("Я жив 🟢")
+async def start(message: Message):
+    await message.answer(
+        "Здравствуйте!\n\n"
+        "Это бот детского модельного агентства 🧸✨\n\n"
+        "Выберите нужный раздел в меню 👇"
+    )
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 if name == "__main__":
     asyncio.run(main())
