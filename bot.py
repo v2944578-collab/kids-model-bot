4e7f4546341d23b47ff556859ebf0bdefc1a28df
v2944578-collab <v2944578@gmail.com>
2025-12-23 07:43:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
+import os
+from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-BOT_TOKEN = "ТВОЙ_ТОКЕН_ЗДЕСЬ"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
